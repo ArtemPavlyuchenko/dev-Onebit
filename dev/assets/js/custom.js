d7570a35e0352bf2150f2aafb62c932182d5c6d8
ArtemPavlyuchenko <artempavlyuchenko03@gmail.com>
2025-01-08 21:27:16 +0200

    comment

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,10 @@
-  
+$(document).ready(function() {
+    $('[type="tel"]').mask('+38(000)-00-00-000')
+    $('#password').hideShowPassword({
+        innerToggle: true
+  });
+})
+
+const btnHistoryBack = document.querySelector('#history-back');
+const btnHistory =()=> window.history.back();
+btnHistoryBack.addEventListener('click', btnHistory)
