581e3415570636ebc9ab1ffad8104f251cab8a81
ArtemPavlyuchenko <artempavlyuchenko03@gmail.com>
2025-01-28 14:34:15 +0200

    comment

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -7,4 +7,13 @@ $(document).ready(function() {
 
 const btnHistoryBack = document.querySelector('#history-back');
 const btnHistory =()=> window.history.back();
-btnHistoryBack.addEventListener('click', btnHistory)
+if (btnHistoryBack) btnHistoryBack.addEventListener('click', btnHistory)
+
+window.addEventListener("scroll", function() {
+  const header = document.querySelector("header");
+  if (window.scrollY > header.offsetTop) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
